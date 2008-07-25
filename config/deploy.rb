d5b7dbd27f4241e606d90aca7e4059e30896c86a
Miriam Goldberg <mag@skyhawk.(none)>
2008-07-25 13:28:20 -0400

    initial capistrano files. not working yet.

---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+set :application, "CiteParse"
+set :repository,  "git@github.com:publicdisplay/citeparse.git"
+set :branch, "master"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "your app-server here"
+role :web, "your web-server here"
+role :db,  "your db-server here", :primary => true
